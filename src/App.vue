a66be32c89bc918970cc679562bf4efb9b45924b
Nadelopo <mistrmorsikk@mail.ru>
2024-10-07 19:39:27 +0400

    feat: add logout btn

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-import { VThemeProvider, VContainer } from 'vuetify/components'
+import { useRoute, useRouter } from 'vue-router'
+import { VThemeProvider, VContainer, VBtn } from 'vuetify/components'
+
+const route = useRoute()
+const router = useRouter()
+
+const onLogout = () => {
+  router.push({ name: 'Auth' })
+}
 </script>
 
 <template>
   <v-theme-provider theme="dark" with-background>
     <v-container>
+      <div v-if="route.name && route.name !== 'Auth'" class="text-end mb-5">
+        <v-btn @click="onLogout">Выйти</v-btn>
+      </div>
       <RouterView />
     </v-container>
   </v-theme-provider>
